2aabe34763ce0c816f4f853d71f979d472908326
wanzenried <wanzenried@users.noreply.github.com>
2020-03-18 16:16:10 +0100

    removed hardcoding

--- Buttons.js
+++ Buttons.js
@@ -2,8 +2,8 @@ class betterButton {
   constructor(x,y,w,h,fun,args) {
     this.button = createButton();
     this.button = createButton("test");
-    this.button.position(5,5);
-    this.button.size(40,40);
+    this.button.position(x,y);
+    this.button.size(w,h);
     this.button.style("color","Transparent")
     this.button.style("background-color","Transparent")
     this.button.style("border", "none");
